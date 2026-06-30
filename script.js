38d7e871a6755e846e5c4add85762253d62a97c9
MasterRoachi <207052285+MasterRoachi@users.noreply.github.com>
2026-06-30 19:05:38 +0200

    Step 3 - Rock, Paper, Scissors Game
    added player score and computer score variables to keep track of the score throughout the game.
    added playRound function to play a single round of the game, which takes in the player's choice and the computer's choice as arguments and determines the winner of that round.

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 const options = ['quartz', 'parchment', 'shears'];
 
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice () {
     const randomIndex = Math.floor(Math.random() * options.length);
     const computerChoice = options[randomIndex];
     return computerChoice;
+}
+
+function getHumanChoice () {
+    const humanChoice = prompt ("What shall it be - Quartz, Parchment, or Shears?");
+    return humanChoice.toLowerCase();
+}
+
+function playRound (humanChoice, computerChoice) {
+    const human = humanChoice.toLowerCase();
+    const computer = computerChoice.toLowerCase();
+
+    if (human === computer) {
+        console.log("Dang Flabbit, We Drew!");
+    } else if ((human === "quartz" && computer === "shears") ||
+               (human === "parchment" && computer === "quartz") ||
+               (human === "shears" && computer === "parchment")) {
+        humanScore++;
+        console.log(`Jolly good show ${human} triumphs over ${computer}`);
+    } else {
+        computerScore++;
+        console.log(`Alas! ${computer} truimphs over ${human}`);
+    }
 }
